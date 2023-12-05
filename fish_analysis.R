c2a2db58c35f2c4e22d435a557a9a9da363d8483
Bharti Parihar <bhartologist@gmail.com>
2023-12-05 18:29:46 +0000

    Move data file into subdirectory

--- fish_analysis.R
+++ fish_analysis.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 
 fish_data_cat <- fish_data |>
   mutate(length_cat <- ifelse(length > 300, "big", "small"))
